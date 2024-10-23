7958e288cb237c19b602d03c734b3f3354145873
Dafianeseif <seifdaf@gmail.com>
2024-10-23 18:14:09 +0100

    Ajout du dossier 'portfolio_tail'

---
+++ portfolio_tail/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-red': '#7e0000',
+        'custom-dark-red': '#d30023',
+      },
+    },
+  },
+  plugins: [],
+}
+
